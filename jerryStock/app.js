fe0ea1fad8181159cac73f5f222cfc2868682aab
Chufan Deng <jerry.dengcf@gmail.com>
2017-11-09 16:04:54 -0800

    node api 1

--- jerryStock/app.js
+++ jerryStock/app.js
@@ -18,10 +18,36 @@
 // [START app]
 var express = require('express');
 var app = express();
+var request = require('request');
+
+var parseString = require('xml2js').parseString;
+var cors = require('cors');
+var url = require("url");
 // app.set('view engine', 'html');
+app.use(cors());
+var url_stock = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
+var symbol;
 
 app.get('/', function(req, res){
     res.status(200).sendfile('./index.html');
+
+    var myAPI_stock = "&apikey=OE0QXT6U0BKFHVV8";
+    var params = url.parse(req.url,true).query;
+    symbol = params.symbol;
+     // console.log(symbol);
+    url_stock = url_stock + symbol + myAPI_stock;
+
+});
+app.get('/s', function(req, res) {
+
+    // var params = url.parse(req.url,true).query;
+    // symbol = params.symbol;
+    console.log(symbol);
+    console.log(url_stock);
+    request(url_stock, function(error, response, body) {
+        res.send(body);
+    });
+
 });
 
 // Start the server
