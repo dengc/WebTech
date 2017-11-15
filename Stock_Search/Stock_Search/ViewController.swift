06e8e548e327b566bb21f693d0f0ffaaf29d8d96
Chufan Deng <jerry.dengcf@gmail.com>
2017-11-14 16:21:03 -0800

    ios development start

---
+++ Stock_Search/Stock_Search/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stock_Search
+//
+//  Created by 邓楚凡 on 14/11/2017.
+//  Copyright © 2017 邓楚凡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myLabel.text = "haha"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
